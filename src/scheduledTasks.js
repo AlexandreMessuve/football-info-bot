b154b9212899d8f7b246d463d038c702bff28d3f
Alexandre Messuve <alexandre.messuves@gmail.com>
2025-08-23 09:45:12 +0200

    fix

--- src/scheduledTasks.js
+++ src/scheduledTasks.js
@@ -52,7 +52,7 @@ export async function postWeeklyOverviews(client) {
                     let embed = new EmbedBuilder()
                         .setColor('#0099ff')
                         .setTitle(`📅 Programme - ${competitionName}`)
-                        .setThumbnail(matchChunk[0].league.logo)
+                        .setThumbnail(matchChunk[0][0].league.logo)
                         .setTimestamp();
 
                     for (const match of matchChunk) {
@@ -103,7 +103,7 @@ export async function updateAllScores(client) {
                         let newEmbed = new EmbedBuilder()
                             .setColor('#0099ff')
                             .setTitle(`📅 Programme - ${competitionName}`)
-                            .setThumbnail(chunkMatches[0].league.logo)
+                            .setThumbnail(chunkMatches[0][0].league.logo)
                             .setTimestamp();
 
                         for (const match of chunkMatches) {
